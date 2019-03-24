a7d0ba5cac73b0dc704a74ca64904fc02354f466
Aaron Mackall <aaronmckll@gmail.com>
2019-03-24 17:17:23 -0600

    completed

--- practice.js
+++ practice.js
@@ -27,7 +27,10 @@
   Then invoke the callback function, passing in the first element in the array as it's argument.
 */
 
-// Code Here 
+function first(arr, callback) {
+    callback(arr[0]);
+}
+
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -47,7 +50,9 @@ first(names, function(firstName){
   Then invoke the callback, passing in the last element in the array as the argument.
 */
 
-//Code Here
+function last(arr, callback) {
+  callback(arr[arr.length - 1]);
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -65,7 +70,9 @@ last(names, function(lastName){
   Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
 */
 
-//Code Here
+function multiply(num1, num2, callback) {
+  callback(num1 * num2);
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -84,7 +91,16 @@ multiply(4, 3, function(answer){
   If the name does not exist, invoke the callback with false as the argument.
 */
 
-//Code Here 
+function contains(arr, name, callback){
+  for(i = 0; i < arr.length; i++){
+  if (name === arr[i]) {
+        callback(true);
+  } else {
+     callback(false);
+  }
+ }
+} 
+
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -105,7 +121,18 @@ contains(names, 'Colt', function(result){
   Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
 */
 
-//Code Here
+
+
+function uniq(arr, callback) {
+  for(i = 0; i < arr.length; i++) {
+    for(l = 0; l < arr.length; l++) {
+     if (i != l && arr[i] === arr[l]){ 
+       arr.splice(i,1)
+   }
+  }
+ }
+ callback(arr);
+}
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -122,7 +149,11 @@ uniq(names, function(uniqArr){
   For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
 */
 
-//Code Here 
+function each(arrNames, callback) {
+  for(i = 0; i < arrNames.length; i++) {
+    callback(arrNames[i],i);
+  }
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -140,6 +171,13 @@ each(names, function(item, indice){
 */
 
 // Code here
+function getUserById(users, id, callback) {
+for(i = 0; i < users.length; i++) {
+  if(users[i].id === id) {
+  }
+  return callback(users[i]);
+ }
+}
 
 // Do not edit the code below.
 var users = [
